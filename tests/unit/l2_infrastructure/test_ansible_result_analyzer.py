66d822e90887325114961d1e0b82735001441f94
Philipp Hossner <philipp.hossner@posteo.de>
2022-02-23 18:20:29 +0100

    WIP Add app state check via Ansible

--- tests/unit/l2_infrastructure/test_ansible_result_analyzer.py
+++ tests/unit/l2_infrastructure/test_ansible_result_analyzer.py
@@ -125,6 +125,133 @@ ANSIBLE_RESULT_NOT_INSTALLED = """
 }
 """
 
+ANSIBLE_RESULT_INSTALLED = """
+{
+    "custom_stats": {},
+    "global_custom_stats": {},
+    "plays": [
+        {
+            "play": {
+                "duration": {
+                    "end": "2022-02-23T13:36:48.154701Z",
+                    "start": "2022-02-23T13:36:47.962341Z"
+                },
+                "id": "2b703211-0506-cc3a-1ba1-000000000005",
+                "name": "Check Cowsay Status"
+            },
+            "tasks": [
+                {
+                    "hosts": {
+                        "localhost": {
+                            "_ansible_no_log": false,
+                            "action": "command",
+                            "changed": false,
+                            "cmd": [
+                                "which",
+                                "cowsay"
+                            ],
+                            "delta": "0:00:00.001775",
+                            "end": "2022-02-23 14:36:48.126090",
+                            "failed_when_result": false,
+                            "invocation": {
+                                "module_args": {
+                                    "_raw_params": "which cowsay",
+                                    "_uses_shell": false,
+                                    "argv": null,
+                                    "chdir": null,
+                                    "creates": null,
+                                    "executable": null,
+                                    "removes": null,
+                                    "stdin": null,
+                                    "stdin_add_newline": true,
+                                    "strip_empty_ends": true,
+                                    "warn": false
+                                }
+                            },
+                            "rc": 0,
+                            "start": "2022-02-23 14:36:48.124315",
+                            "stderr": "",
+                            "stderr_lines": [],
+                            "stdout": "/usr/games/cowsay",
+                            "stdout_lines": [
+                                "/usr/games/cowsay"
+                            ]
+                        }
+                    },
+                    "task": {
+                        "duration": {
+                            "end": "2022-02-23T13:36:48.138154Z",
+                            "start": "2022-02-23T13:36:47.968086Z"
+                        },
+                        "id": "2b703211-0506-cc3a-1ba1-000000000007",
+                        "name": "Check if cowsay executable in in PATH"
+                    }
+                },
+                {
+                    "hosts": {
+                        "localhost": {
+                            "_ansible_no_log": false,
+                            "_ansible_verbose_always": true,
+                            "action": "debug",
+                            "changed": false,
+                            "msg": "ANSIBLE_SELF_SERVICE_STATUS_INSTALLED"
+                        }
+                    },
+                    "task": {
+                        "duration": {
+                            "end": "2022-02-23T13:36:48.147060Z",
+                            "start": "2022-02-23T13:36:48.139926Z"
+                        },
+                        "id": "2b703211-0506-cc3a-1ba1-000000000008",
+                        "name": "Signal status installed"
+                    }
+                },
+                {
+                    "hosts": {
+                        "localhost": {
+                            "_ansible_no_log": false,
+                            "action": "debug",
+                            "changed": false,
+                            "skip_reason": "Conditional result was False",
+                            "skipped": true
+                        }
+                    },
+                    "task": {
+                        "duration": {
+                            "end": "2022-02-23T13:36:48.154701Z",
+                            "start": "2022-02-23T13:36:48.148595Z"
+                        },
+                        "id": "2b703211-0506-cc3a-1ba1-000000000009",
+                        "name": "Signal status not installed"
+                    }
+                }
+            ]
+        },
+        {
+            "play": {
+                "duration": {
+                    "start": "2022-02-23T13:36:48.158565Z"
+                },
+                "id": "2b703211-0506-cc3a-1ba1-00000000000a",
+                "name": "Install Cowsay"
+            },
+            "tasks": []
+        }
+    ],
+    "stats": {
+        "localhost": {
+            "changed": 0,
+            "failures": 0,
+            "ignored": 0,
+            "ok": 2,
+            "rescued": 0,
+            "skipped": 1,
+            "unreachable": 0
+        }
+    }
+}
+"""
+
 
 def test_not_installed(mocker):
     ansible_result_mock = mocker.Mock()
@@ -132,3 +259,11 @@ def test_not_installed(mocker):
     analyzer = JMESPathAnsibleResultAnalyzer()
     assert analyzer.signaling_not_installed(ansible_result_mock) is True
     assert analyzer.signaling_installed(ansible_result_mock) is False
+
+
+def test_installed(mocker):
+    ansible_result_mock = mocker.Mock()
+    ansible_result_mock.data = json.loads(ANSIBLE_RESULT_INSTALLED)
+    analyzer = JMESPathAnsibleResultAnalyzer()
+    assert analyzer.signaling_not_installed(ansible_result_mock) is False
+    assert analyzer.signaling_installed(ansible_result_mock) is True
